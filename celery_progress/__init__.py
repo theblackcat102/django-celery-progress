cd426db187c44b24262e1854a1f78fa5dc154f47
theblackcat102 <zhirui09400@gmail.com>
2018-04-18 13:39:08 +0800

    fix django version bug

--- celery_progress/__init__.py
+++ celery_progress/__init__.py
@@ -1,12 +1,19 @@
 from django.conf import settings
-from django.utils.module_loading import import_by_path
+
+import django
+from distutils.version import LooseVersion, StrictVersion
+
+if StrictVersion(django.get_version()) >= StrictVersion("1.7"):
+    from django.utils.module_loading import import_string
+else:
+    from django.utils.module_loading import import_by_path as import_string
 
 BACKEND = getattr(settings, 'CELERY_PROGRESS_BACKEND',
                   'celery_progress.backends.CeleryBackend')
 
 
 def get_backend():
-    return import_by_path(BACKEND)
+    return import_string(BACKEND)
 
 
 backend = get_backend()()
